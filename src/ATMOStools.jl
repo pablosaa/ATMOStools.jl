5316e30d9211af2e1c3b98c6ae879cb312cec6b6
Pablo Saavedra-Garfias <pablosaa@uni-bonn.de>
2020-12-23 21:07:35 +0100

    Create ATMOStools.jl

---
+++ src/ATMOStools.jl
@@ -0,0 +1,11 @@
+module ATMOStools
+
+using Printf
+
+"""
+ATMOStools a set of functions and constant useful to work within the atmospheric physics and meteorology.
+
+"""
+ATMOStools
+
+end #module
