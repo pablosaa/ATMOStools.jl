53597553735981888dc7e11d7d9927932c4b8b22
Pablo Saavedra Garfias <pablosaa@uni-bonn.de>
2022-06-28 05:29:46 +0200

    added uncertainty function for surface temperature

--- src/radiation.jl
+++ src/radiation.jl
@@ -9,17 +9,37 @@
 Function to estimate the surface temperature give the LW downwelling
 and upwelling radiation fluxes.
 
-> Tₛ = T_surf(F_dw, F_up)
+julia> Tₛ = T_surf(F_dw, F_up)
+
+or, giving a specific emissivity value of 0.96:
+
+julia> Tₛ = T_surf(F_dw, F_up, εₛ=0.96)
 
 with Tₛ in K.
 
-Optional parameter is ϵₛ the surface emissivity (default value = 0.98)
+Optional parameter is εₛ the surface emissivity (default value = 0.985)
 """
-function T_surf(Flw_dw, Flw_up; ϵₛ = 0.98)
+function T_surf(Flw_dw, Flw_up; εₛ = 0.985)
     #where σ = 5.670374419E−8 # [W⋅m−2⋅K−4] Stephan-Boltzman constant
     # and ϵₛ the surface emissivity:
-    return ((Flw_up - (1-ϵₛ)*Flw_dw)/ϵₛ/σ)^0.25
+    return ((Flw_up - (1-εₛ)*Flw_dw)/εₛ/σ)^0.25
 end
 
+"""
+Uncertainty function for T_surf due to emissivity
 
+julia> δTₛ = δT_surf(Flw_dw, Flw_up, δε)
+
+with δε the error in percentage e.g. (Δε/ε).
+The optional emissivity variable has a default value of εₛ = 0.985
+
+"""
+function δT_surf(Flw_dw, Flw_up, δε; εₛ = 0.985)
+    faktor0 = let Tₛ= T_surf(Flw_dw, Flw_up, εₛ = εₛ)
+        1/4/(Tₛ)^3
+    end
+    faktor1 = (Flw_up - Flw_dw)/εₛ/σ
+    
+    return faktor0*faktor1*δε
+end
 # end of file
