1644c5fd826d643bd65d2661d30c6afe3f709f31
Pablo Saavedra Garfias <pablosaa@uni-bonn.de>
2023-01-23 21:43:48 +0100

    fix bug typeof(rs_height) -> eltype(rs_height).. duh!

--- src/boundary_layer.jl
+++ src/boundary_layer.jl
@@ -150,7 +150,7 @@ function cloud_decoupling_height(rs_height::Vector, CBH::Vector, θᵥ::Matrix; 
 end
 function cloud_decoupling_height(rs_height::Vector, CBH::Matrix, θᵥ::Matrix; θ_thr=0.025, topmixlayer=false)
     ntime, nlayer = size(CBH)
-    decop_hgt = Matrix{typeof(rs_height)}(undef, ntime, nlayer)
+    decop_hgt = Matrix{eltype(rs_height)}(undef, ntime, nlayer)
     for i ∈ (1:nlayer)
         decop_hgt[:, i] = cloud_decoupling_height(rs_height, CBH[:,i], θᵥ, θ_thr=θ_thr, topmixlayer=topmixlayer)
     end
